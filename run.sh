7cc79c0cf9b0c34a698bd4b929378256f356369b
jennyyuejin <jin.yuejenny@gmail.com>
2016-10-11 16:40:03 -0400

    fixed wrong mongodb db name

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-MONGO_URL=mongodb://localhost:27017/meteor-tutorial-todos meteor --port $1 run
+MONGO_URL=mongodb://localhost:27017/react-meteor-boilerplate meteor --port $1 run
 
