1f13f32895db253c4182f25a119d34a3def02542
j-planet <jin.yuejenny@gmail.com>
2016-12-03 17:33:38 -0800

    added server stuff, email templates

--- server/main.jsx
+++ server/main.jsx
@@ -1,14 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { SomeCollection } from '../api/some_api';
+
+import { MAILGUN_URL } from './secrets';
 
 
 Meteor.startup(() =>
 {
-    // code to run on the server at startup
-    // can NOT use a fat arrow, which wouldn't let us do this.userid
-    Meteor.publish('someCollection', function()
-    {
-        return SomeCollection.find( { owner: this.userId } );
-    });
-
+    process.env.MAIL_URL = MAILGUN_URL;
 });
