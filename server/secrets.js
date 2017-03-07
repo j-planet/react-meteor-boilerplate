8a567c9ee5fb844ea56f323a469204148098d7b3
j-planet <jin.yuejenny@gmail.com>
2017-03-06 19:36:55 -0800

    updated gitignore

--- server/secrets.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by jj on 10/12/16.
- */
-
-exports.MAILGUN_URL = "smtp://postmaster%40sandbox74e60cf4994f467b98ab9c21194575f7.mailgun.org:1e86c3e81054addfece40a72ca9741ea@smtp.mailgun.org:587";
-
